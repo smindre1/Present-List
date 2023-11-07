868a583d748009107ea5dad2a8fff43eb3d7107c
Shane Mindreau <smindre1@binghamton.edu>
2023-11-07 14:51:09 -0500

    I have created a save button that when clicked, will update all the changes the user made to the table/list on the database. I still need to include the title that I added to be affected by the save function.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const presentRoutes = require('./presentRoutes');
+const saveRoutes = require('./saveRoutes');
 
 router.use('/users', userRoutes);
 router.use('/present', presentRoutes);
+router.use('/save', saveRoutes);
 
 module.exports = router;
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,41 +2,6 @@ const router = require('express').Router();
 const { Users, Lists, List_Items } = require('./../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', async (req, res) => {
-//   //This is for seeded data
-//   try {
-//     const presentData = await Present.findAll(
-//       /*{
-//       include: [
-//         {
-//           model: Users,
-//           attributes: ['name']
-//         }
-//       ]
-//     }*/);
-
-//     const present = presentData.map((present) => present.get({ plain: true }));
-
-//     res.render('main', {
-//       present,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-//------------Organized Routes for handlebar pages-----------------
-
-const convertData = (rawText, keyName) => {
-  let newArray = [];
-  for (i = 0; i < rawText.length; i++) {
-    newArray.push(keyName);
-  }
-  console.dir('New Array Data:');
-  console.dir(newArray);
-};
-
 //The mainpage. Usually homepage.
 router.get('/', async (req, res) => {
   //Should create a homepage to explain sites purpose. Replace login with new handlebar
@@ -103,39 +68,12 @@ router.get('/List/:id', async (req, res) => {
     });
 
     //const listD = listData.map((list) => list.get({ plain: true }));
-    console.log('list Data', listData);
+    // console.log('list Data', listData);
     res.json(listData);
 
-    /*res.render('profile', {
-      listData,
-      logged_in: req.session.logged_in
-    });*/
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
-//Gets the individual present data
-// router.get('/Present/:id', async (req, res) => {
-//   try {
-//     const presentData = await List_Items.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: List_Items
-//           //attributes: ['name']
-//         }
-//       ]
-//     });
-
-//     const present = presentData.get({ plain: true });
-
-//     res.render('present', {
-//       ...present,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 module.exports = router;
