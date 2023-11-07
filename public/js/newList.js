5fb0f5167f01285ae296e74574c4bc4a8fe58ae8
Affan Kamal <affan.kamal91@gmail.com>
2023-11-07 17:43:22 -0500

    updated + list button

--- public/js/newList.js
+++ public/js/newList.js
@@ -1,17 +1,32 @@
-const createNewList = async (event) => {
+let createListSection = document.getElementById("createList")
+console.log(createListSection)
+const showListSection = async (event) => {
+
+ 
+  console.log(createListSection)
+  createListSection.classList.remove("hide")
+  var saveButton = document.getElementById("saveList")
+  saveButton.addEventListener("click", saveNewList)
+};
+const saveNewList = async (event) =>{
   event.preventDefault();
+  console.log("in the createnewlist!!!")
+  let inputValue = document.getElementById("listValue")
+  console.log(inputValue.value)
   const response = await fetch('/api/newList', {
     method: 'POST',
-    body: JSON.stringify({ title: 'New List!' }),
+    body: JSON.stringify({ title: inputValue.value}),
     headers: { 'Content-Type': 'application/json' }
   });
 
   if (response.ok) {
+    createListSection.classList.add("hide")
+    document.location.replace('/profile');
   } else {
     alert(response.statusText);
   }
-};
+}
 
 document
   .querySelector('#newListButton')
-  .addEventListener('click', createNewList);
+  .addEventListener('click', showListSection);
