35251c1d423fbef5106e1066dcc1f2c1ead26460
Shane Mindreau <smindre1@binghamton.edu>
2023-11-07 00:29:44 -0500

    I made it so when a user clicks on one of their lists from the tab on the top right of the interface, the data for that list will become displayed on a table in the center of the page. Clicking on a different list will close the previous list table and load the newly selected list's data.

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,14 @@
 USE present_db;
 SELECT * FROM lists;
 
+INSERT INTO users (username, email, password)
+VALUES ("user", "user@gmail.com", "password");
 
 INSERT INTO lists (title, theme, user_id)
-VALUES ("second", Null, 1);
+VALUES ("List Title", Null, 1), ("Empty", Null, 2);
+
+INSERT INTO list_items (recipient, price, present, date, list_id)
+VALUES ("Bob", "3:00$", "Present 1", Null, 1, ),
+("Jerry", "6:00$", "Present 2", Null, 1, ),
+("Tom", "9:00$", "Present 3", Null, 1, ),
+("Chuck", "12:00$", "Present 4", Null, 1, );
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,25 +1,75 @@
 const displayList = async (event) => {
   event.preventDefault();
 
-  const id = event.currentTarget.getAttribute("listid");
-  console.log(id, "id");
+  const id = event.currentTarget.getAttribute('listid');
+  console.log(id, 'id');
 
   const response = await fetch(`/List/${id}`);
 
   if (response.ok) {
-    console.log("response 200");
+    console.log('response 200');
     const data = await response.json();
-    console.log(data)
-    // let table = document.querySelector('#listTable')
-    // let newTable = table.appendChild.createElement("p");
-    // newTable.innerHTML = "Testing 1, 2 3...";
-    // table.setAttribute('id', 'table');
-    // table.append("Testing 1, 2, 3...");
+    console.log(data);
+
+    let table = document.querySelector('#listTable');
+    //Clears any previous list displayed
+    let child = table.lastElementChild;
+    while (child) {
+      table.removeChild(child);
+      child = table.lastElementChild;
+    }
+
+    const keyValues = ['recipient', 'price', 'present', 'date'];
+    const titles = ['Recipient', 'Price', 'Present', 'Date'];
+
+    //Creates the div container for all the user's list column titles
+    let titleRow = document.createElement('div');
+    titleRow.setAttribute('id', 'titleRow');
+    titleRow.setAttribute('class', 'flexRow');
+    table.appendChild(titleRow);
+    let destination = document.querySelector('#titleRow');
+    //Creates the titles for all the user's columns
+    for (i = 0; i < 4; i++) {
+      let tableColumnTitle = document.createElement('ul');
+      tableColumnTitle.setAttribute('id', 'column-title');
+      tableColumnTitle.innerHTML = titles[i];
+      destination.appendChild(tableColumnTitle);
+    }
+    //Creates rows based on the data of each list_item in the user's list
+    for (i = 0; i < data.length; i++) {
+      //Creates the container for each item row
+      let tableRow = document.createElement('div');
+      let rowNumber = i + 1;
+      tableRow.setAttribute('id', `row-${rowNumber}`);
+      tableRow.setAttribute('class', `listRow`);
+      table.appendChild(tableRow);
+      //Creates the destination for all the item data to be sent
+      let row = document.querySelector(`#row-${rowNumber}`);
+      //Displays the recipient data for every item of the list
+      let newElement = document.createElement('textarea');
+      newElement.setAttribute('type', 'recipient');
+      newElement.innerHTML = data[i].recipient;
+      row.appendChild(newElement);
+      //Displays the price data for every item of the list
+      let newElementTwo = document.createElement('textarea');
+      newElementTwo.setAttribute('type', 'price');
+      newElementTwo.innerHTML = data[i].price;
+      row.appendChild(newElementTwo);
+      //Displays the present data for every item of the list
+      let newElementThree = document.createElement('textarea');
+      newElementThree.setAttribute('type', 'present');
+      newElementThree.innerHTML = data[i].present;
+      row.appendChild(newElementThree);
+      //Displays the date data for every item of the list
+      let newElementFour = document.createElement('textarea');
+      newElementFour.setAttribute('type', 'date');
+      newElementFour.innerHTML = data[i].date;
+      row.appendChild(newElementFour);
+    }
   } else {
     alert('Failed to create project');
   }
 };
-
 
 const updateList = async (event) => {
   event.preventDefault();
@@ -46,7 +96,6 @@ const updateList = async (event) => {
     }
   }
 };
-
 
 const updateListItems = async (event) => {
   event.preventDefault();
@@ -90,13 +139,11 @@ const delButtonHandler = async (event) => {
   }
 };
 
-const ListButtons = document
-.querySelectorAll('.listBtn');
+const ListButtons = document.querySelectorAll('.listBtn');
 
 ListButtons.forEach(function(button) {
   button.addEventListener('click', displayList);
 });
-
 
 // document
 //   .querySelector('.project-list')
